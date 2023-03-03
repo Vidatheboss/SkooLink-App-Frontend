8b5aab5db95d09e53074879c731b534908f9624e
Phelipe Matias <phelipems91@gmail.com>
2023-03-03 15:53:45 -0500

    Created login page, routes, and header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfilesComponent } from './pages/profiles/profiles.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
+
+const routes: Routes = [
+  { path:'home', component: HomeComponent },
+  { path:'login', component: LoginComponent },
+  { path:'profiles', component: ProfilesComponent },
+  { path:'signup', component: SignupComponent },
+  { path:'', redirectTo: 'login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  constructor(private router: Router) { }
+
+  username: string | undefined;
+  password: string | undefined;
+
+  ngOnInit() {
+  }
+
+  login(): void {
+    
+  }
 }
