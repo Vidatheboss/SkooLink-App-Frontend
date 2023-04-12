e2f95b15d3827ae149888857cc1e98a4fdbb83b2
Phelipe Matias <phelipems91@gmail.com>
2023-04-12 18:38:32 -0400

    Student service unit test added

--- src/app/services/student.service.spec.ts
+++ src/app/services/student.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { StudentService } from './student.service';
 
-describe('StudentService', () => {
+describe('studentService', () => {
   let service: StudentService;
-
+  let httpMock: HttpTestingController;
+  
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(StudentService);
+      TestBed.configureTestingModule({
+          imports: [HttpClientTestingModule],
+          providers: [StudentService]
+      });
+      service = TestBed.get(StudentService);
+      httpMock = TestBed.get(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should be able to retrieve student', () => {
+    const data = Object({ name: 'Bruna Donatoni' });
+
+    service.getStudent(2).subscribe(student => {
+        expect(student).toBe(data);
+    });
+
+    const request = httpMock.expectOne('http://localhost:8080/students/profile/2');
+    expect(request.request.method).toBe('GET');
+    request.flush(data);
   });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
 });
